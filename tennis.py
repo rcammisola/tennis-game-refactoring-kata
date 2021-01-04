edc51b4171288eb247fc7ef596a1490bad915ac9
rcammisola <rocco.cammisola@performgroup.com>
2021-01-04 01:24:09 +0000

    Renamed single character variable names

--- tennis.py
+++ tennis.py
@@ -151,19 +151,19 @@ class TennisGame3:
         self.player1_points = 0
         self.player2_points = 0
 
-    def won_point(self, n):
-        if n == self.player1_name:
+    def won_point(self, player_name):
+        if player_name == self.player1_name:
             self.player1_points += 1
         else:
             self.player2_points += 1
 
     def score(self):
         if (self.player1_points < 4 and self.player2_points < 4) and (self.player1_points + self.player2_points < 6):
-            p = ["Love", "Fifteen", "Thirty", "Forty"]
-            s = p[self.player1_points]
-            return s + "-All" if (self.player1_points == self.player2_points) else s + "-" + p[self.player2_points]
+            point_descriptions = ["Love", "Fifteen", "Thirty", "Forty"]
+            score = point_descriptions[self.player1_points]
+            return score + "-All" if (self.player1_points == self.player2_points) else score + "-" + point_descriptions[self.player2_points]
         else:
             if (self.player1_points == self.player2_points):
                 return "Deuce"
-            s = self.player1_name if self.player1_points > self.player2_points else self.player2_name
-            return "Advantage " + s if ((self.player1_points - self.player2_points) * (self.player1_points - self.player2_points) == 1) else "Win for " + s
+            score = self.player1_name if self.player1_points > self.player2_points else self.player2_name
+            return "Advantage " + score if ((self.player1_points - self.player2_points) * (self.player1_points - self.player2_points) == 1) else "Win for " + score
